75b089e944197ad9bf22592f7362897a964185d6
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2026-02-07 10:37:18 +0100

    Secret removed

--- supabase/functions/create-student/index.ts
+++ supabase/functions/create-student/index.ts
@@ -18,15 +18,3 @@ Deno.serve(async (req) => {
     { headers: { "Content-Type": "application/json" } },
   )
 })
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/create-student' \
-    --header 'Authorization: Bearer eyJhbGciOiJFUzI1NiIsImtpZCI6ImI4MTI2OWYxLTIxZDgtNGYyZS1iNzE5LWMyMjQwYTg0MGQ5MCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjIwODQxODkyNjh9.3T7UguZc4RAHIv-uQPQQiqP4ZCYCQPhzL-bw-WjOCqosPFwkXxOodhqQTl6SeF-L3PtJ783HCB_CuHRedDr3_A' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
